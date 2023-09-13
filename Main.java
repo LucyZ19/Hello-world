f0b46c3c9995b62e432987a183eca1c76e19c42d
LucyZ19 <144463503+LucyZ19@users.noreply.github.com>
2023-09-13 10:07:08 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main
+{
+  public static void main (String[] args)
+  {
+    System.out.println ("Hello, world!");
+    System.out.println ("Nice to meet you.");
+    
+    int num = 2;    // int - which store integers (numbers like 3, -76, 20393)                                      //final can be used in front of a variable declaration to make it a constant that cannot be changed
+    double num1 = 2.25;       // double - which store floating point numbers (decimal numbers like 6.3 -0.9, and 60293.93032)
+    double num2 = 1.5;
+    final boolean real = true;   // boolean - which store Boolean values (either true or false).
+    
+    System.out.println ("is " + num1 + " > " + num + "?");
+    System.out.println (real);
+    
+    num2 = num + num2;
+    num1 = (num2 + num) * num;
+    System.out.println ("is " + num1 + " > " + num2 + "?");
+    System.out.println (real);
+  }
+}
