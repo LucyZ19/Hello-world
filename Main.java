882fde57b53ab3bd8b0f2a63655102bc25019970
LucyZ19 <lucyz19@nycstudents.net>
2023-09-19 09:48:30 -0400

    updated

--- Main.java
+++ Main.java
@@ -9,17 +9,17 @@ public class Main
 
     System.out.println("Solve these math problems:");
     Scanner scan = new Scanner(System.in);
-    String num3 = scan.nextLine();
-    System.out.println(num3 + " nice.");
-    scan.close();
+
     
     int num = 2;    // int - which store integers (numbers like 3, -76, 20393)                                      //final can be used in front of a variable declaration to make it a constant that cannot be changed
     double num1 = 2.25;       // double - which store floating point numbers (decimal numbers like 6.3 -0.9, and 60293.93032)
     double num2 = 1.5;
     final boolean real = true;   // boolean - which store Boolean values (either true or false).
     
-    System.out.println ("is " + num1 + " > " + num + "?");
-    System.out.println (real);
+    System.out.println ("true or false is " + num1 + " > " + num + "?");
+    String num3 = scan.nextLine();
+    System.out.println("You answered: " + num3);
+    System.out.println ("The correct answer is " + real);
     
     num2 = num + num2;
     num1 = (num2 + num) * num;
@@ -35,5 +35,7 @@ public class Main
     num2--;
     System.out.println ("is " + num2 + " > " + num1 + "?");
     System.out.println (real);
+
+    scan.close();
   }
 }
