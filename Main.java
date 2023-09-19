87b30ba64c6028527786adc16ff45fe5223138b3
LucyZ19 <lucyz19@nycstudents.net>
2023-09-19 10:08:25 -0400

    updated

--- Main.java
+++ Main.java
@@ -10,11 +10,11 @@ public class Main
     System.out.println("Solve these math problems:");
     Scanner scan = new Scanner(System.in);
 
-    
-    int num = 2;    // int - which store integers (numbers like 3, -76, 20393)                                      //final can be used in front of a variable declaration to make it a constant that cannot be changed
+
+    final int num = 2;    // int - which store integers (numbers like 3, -76, 20393)                                      //final can be used in front of a variable declaration to make it a constant that cannot be changed
     double num1 = 2.25;       // double - which store floating point numbers (decimal numbers like 6.3 -0.9, and 60293.93032)
     double num2 = 1.5;
-    final boolean real = true;   // boolean - which store Boolean values (either true or false).
+    boolean real = true;   // boolean - which store Boolean values (either true or false).
     
     System.out.println ("true or false is " + num1 + " > " + num + "?");
     String num3 = scan.nextLine();
@@ -37,5 +37,10 @@ public class Main
     System.out.println (real);
 
     scan.close();
+
+    double number = 28.0 / 25;
+    System.out.println("28.0/25.0 = " + number);
+    System.out.println("28.0/25.0 truncated: " + (int) number);
+
   }
 }
