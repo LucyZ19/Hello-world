2eac71317d4ad3ccc06dadad146af1ab9ae8f219
LucyZ19 <144463503+LucyZ19@users.noreply.github.com>
2023-10-03 10:10:38 -0400

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator //(double n, int k, String s)
+{
+   // n = num;
+   // k = numKeys;
+   // s = sound;
+    
+    private double num;
+    private double numKeys;
+    private String sound;
+    
+    
+    public double newNum()
+    {
+        return (num + numKeys);
+    }
+    public void newKeys ()
+    {
+        double newNum = 50;
+    }
+}
