d2e9af0159860f2bbc6c04364dfc77dde7268e6b
LucyZ19 <144463503+LucyZ19@users.noreply.github.com>
2023-10-11 10:11:50 -0400

    Add files via upload

--- Game.java
+++ Game.java
@@ -7,13 +7,21 @@ public class Game
     System.out.println("Would you like to play a game? yes or no?");
     Scanner scan = new Scanner(System.in);
     String answer = scan.nextLine();
+    boolean vaild = true;
+    //do {
     
-    if (!answer.equals ("yes")) {
-      System.out.println("Nice to meet you.");
-    } //else if (answer = "no") {
-      
-    //}
+    if (answer.equals ("yes")) 
+    {
+      return vaild = true;
+    } 
+    else if (menu.equals("no"))
+    {
+      return valid = false;
+    }
+    //while(!vaild);
     
     
     scan.close();
-  }}
+   
+  }
+  }
