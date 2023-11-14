6eb296a1046c46b2cf54d51979c7bd9fcc2a9a62
LucyZ19 <144463503+LucyZ19@users.noreply.github.com>
2023-11-14 10:21:38 -0500

    Add files via upload

--- Game.java
+++ Game.java
@@ -23,13 +23,13 @@ public class Game
       if (easy.equals("easy")) 
       {
         System.out.println(game + " gardening");
-        System.out.println("the rules are simple" + "\n" + rule.rules(10, 50));
+        System.out.println("the rules are simple" + "\n" + rule.rules(10, 50, easy));
         System.out.println(mode.g(easy));
       }
       else if (easy.equals("hard"))
       {
         System.out.println(game + " gardening");
-        System.out.println("the rules are simple" + "\n" + rule.rules(15, 70));
+        System.out.println("the rules are simple" + "\n" + rule.rules(15, 70, hard));
         System.out.println(mode.g(easy));
       }
       
--- Rules.java
+++ Rules.java
@@ -2,12 +2,16 @@ public class Rules
 {
   private int f;
   private double m;
-  public String rules(int f, double m)
+  private String e;
+  public String rules(int f, double m, String e)
   {
     String s = "you have a flower bed that can hold " + f + " flowers" + "\n" + "you start with $" + m;
     String d = "\n" + "and every math question you get right, you get that amount in $" + "\n" + "every one you get wrong you lose that amount";
     String g = "\n" + "your goal is to fill your flower bed by buying the flowers from the shop";
+    if (
+    String h = "each flower cost $10";
+  
       
-    return s + d + g;
-  }
+    return s + d + g + h;
+ 
 }
